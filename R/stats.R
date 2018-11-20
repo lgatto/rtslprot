551daf995c460803929608d0f654f31727a3b1bc
Laurent <lg390@cam.ac.uk>
2018-11-20 21:14:29 +0100

    add stats file

---
+++ R/stats.R
@@ -0,0 +1,15 @@
+#' @importFrom msmsTests msms.edgeR
+msms_edgeR_test <- function(e,
+                            null.f,
+                            alt.f,
+                            fnm,
+                            test_name) {
+  div <- apply(exprs(e), 2, sum)
+  res <- msms.edgeR(e, alt.f, null.f,
+                    div = div, fnm = fnm)
+  res$adj.p.values <- p.adjust(res$p.value, method = "BH")
+  if (!missing(test_name))
+    names(res) <- paste(names(res), test_name, sep = "_")
+  fData(e) <- cbind(fData(e), res)
+  return(e)
+}
